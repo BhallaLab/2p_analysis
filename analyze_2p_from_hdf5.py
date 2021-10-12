0f1b8406d15e45ab6850507484fb759ad753e73e
bhalla <bhalla@ncbs.res.in>
2021-10-12 17:37:47 +0530

    Much work on analysis, now plots heatmaps of sorted cell activity per session

--- analyze_2p_from_hdf5.py
+++ analyze_2p_from_hdf5.py
@@ -16,6 +16,7 @@ sns.set()
 imagingSessionNames = ['1', '2', '3']
 NUM_FRAMES = 240
 PEAK_HALF_WIDTH = 3
+BLANK_USFRAME_THRESH = 5.0  # # of stdevs beyond which we blank usframe
 hitKernel = np.array( [0.25, 0.5, 0.25] )
 
 
@@ -32,13 +33,22 @@ def main():
     t0 = time.time()
     #pd.read_hdf("store_tl.h5", "table", where=["index>2"])
     p2data = pd.read_hdf(args.filename, "2pData")
+    p2data.sort_index( inplace = True )
     behavData = pd.read_hdf(args.filename, "behavData")
     print( "Time to load = ", time.time() - t0 )
-    addColumns( p2data )
+    frames = addColumns( p2data )
+    print( "Finished adding columns" )
     #sns.pairplot( dataset.loc['G141','20190913'][['prePk1','prePos1','csPk','csPkPos','postPk1','postPos1']], hue='csPk' )
+    #displayPeakHisto( p2data, pkName = "csPk", posName = "csPkPos", numSdev = 3.0, hitRatio = 0.3, mouse = "G141", date = "20190913" )
 
-    #plt.show()
-    return p2data, behavData
+
+    return p2data, behavData, frames
+
+def displayAllFrames( p2data, frames, sortStartFrame = 40, sortEndFrame = -1, usFrame = -1 ):
+    for mouse in p2data.index.levels[0]:
+        for date in p2data.loc[mouse].index.get_level_values(0).unique():
+            print( "Mouse = ", mouse, ", date = ", date )
+            displayPSTH( p2data, frames, sortStartFrame = sortStartFrame, sortEndFrame = sortEndFrame, usFrame = usFrame, mouse = mouse, date = date )
 
 
 def findAndIsolateFramePeak( dfbf2, startFrame, endFrame, halfWidth ):
@@ -59,21 +69,19 @@ def findAndIsolateFramePeak( dfbf2, startFrame, endFrame, halfWidth ):
         peakPos.append( pp + s )
         dfbf2[j, i0:i1] = 0.0
     
+    print( "Found Frame Peak between {} and {}".format( startFrame[0], endFrame[0] ) )
     return np.array( peakVal ), peakPos
 
 
 def addColumns( df ):
-
     y = df["frames"].tolist()
     csFrame = df["csFrame"]
     usFrame = df["usFrame"]
     print( "CSFRAME SHAPE === ", csFrame.shape, usFrame.shape )
     # Here we convert the ragged array of y to a padded array.
     length = max(map(len, y))
-    dfbf2 = np.array([yi+[0.0]*(length-len(yi)) for yi in y])
-    print( "LEEN = ", len( dfbf2 ), len( dfbf2[0] ), len( dfbf2[2000] ) )
-    
-    print( "SHAPE = ", dfbf2.shape, len(dfbf2) )
+    dfbf2 = np.array( [yi+[0.0]*(length-len(yi)) for yi in y] )
+    print( "Finished Padding dfbf array, shape = ", dfbf2.shape )
     # I want to build stdev of values below 80 percentile.
     perc = np.percentile( dfbf2, 80, axis = 1 )
     #np.std(np.ma.masked_where( b > np.repeat(pec,5).reshape( 20, 5 ), b ),1)
@@ -82,10 +90,14 @@ def addColumns( df ):
     maskedDfbf2 = np.ma.masked_where( dfbf2 > np.repeat(perc, ax2 ).reshape( ax1, ax2 ), dfbf2 )
     sd = np.std( maskedDfbf2, 1 )
     mn = np.mean( maskedDfbf2, 1 )
+
     #sd = np.std(np.ma.masked_where( dfbf2 > np.repeat(perc, ax2 ).reshape( ax1, ax2 ), dfbf2 ),1)
     #print( "SHAPE = ", sd.shape, "  DFBF2 = ", dfbf2.shape )
     df['sdev80'] = sd
     df['mean80'] = mn
+    #df['paddedFrames' ] = dfbf2.tolist()
+    print( "Computed mean and SD for dataframe")
+
     zeros = [0] * len( csFrame )
     df['prePk1'], df['prePos1'] = findAndIsolateFramePeak( dfbf2, zeros, csFrame -1, PEAK_HALF_WIDTH )
     df['prePk2'], df['prePos2'] = findAndIsolateFramePeak( dfbf2, zeros, csFrame -1, PEAK_HALF_WIDTH )
@@ -93,7 +105,84 @@ def addColumns( df ):
     df['postPk1'], df['postPos1'] = findAndIsolateFramePeak( dfbf2, usFrame, [len( dfbf2[0] )] * len( dfbf2 ),PEAK_HALF_WIDTH )
     df['postPk2'], df['postPos2'] = findAndIsolateFramePeak( dfbf2, usFrame, [len( dfbf2[0] )] * len( dfbf2 ), PEAK_HALF_WIDTH )
     print( df.head() )
+    return pd.DataFrame( dfbf2.tolist(), index = df.index )
 
+def displayPeakHisto( df, pkName = "csPk", posName = "csPkPos", numSdev = 3.0, hitRatio = 0.2, mouse = "G141", date = "20190913" ):
+    # Generate histo of positions for a given cell on a given session.
+    # Option 1: Add pos for cases where pk/sdev > numSdev
+    # Option 2: Linear weight by pos*(pk/sdev)
+    # Option 3: Like 2 but only show cells that clear numSdev threshold 
+        # for hitRatio fraction of trials.
+    # Sort all histos by peak time.
+    pk = df.loc[(mouse, date)][pkName]
+    pos = df.loc[(mouse, date)][posName]
+    sdev = df.loc[(mouse, date)]["sdev80"]
+    numTrials = len( pk[0] )
+    numCells = len( pk ) // numTrials
+    #################
+    # Option 1
+    sigpos = pos[pk/sdev > numSdev] # Generate cases for option 1.
+    # foo.value_counts() gives a histogram table
+    # sigpos.groupby(level=0).value_counts() gives 71 histos, one per cell
+    # Now how to plot as multiple lines?
+    #################
+    # Option 2: Why not just do a normalized PSTH?
+
+    print( pk )
+    print( pos )
+    print( sdev )
+    return pk, pos, sdev
+
+def displayPSTH( df, frames, sortStartFrame = 40, sortEndFrame = -1, usFrame = -1, mouse = "G141", date = "20190913" ):
+    if sortEndFrame == -1:
+        sortEndFrame = 1000
+    # generate heatMap normalized PSTH (full-frame) for specified session.
+    # Sorted by peak within specified range.
+    # I want to average all the psths for a given cell
+    # I want to average all the sdevs for a given cell
+    # idxmax gives the index of the max value of a colum.
+    # I want to sort by idxmax. Not look up array by idxmax.
+    
+    psth = frames.loc[ (mouse, date) ].mean( axis = 0, level = 0 )
+    sdev = df.loc[(mouse, date)]["sdev80"].mean( axis = 0, level = 0 )
+    ratio = psth.div( sdev, axis = 0 )
+
+    # Check for usFrame as the one which has the highest peak 
+    # averaged over all cells. Use this option if usFrame == -1.
+    if usFrame == -1:
+        mn = ratio.mean( axis = 0 )
+        if mn.max() / mn.std() > BLANK_USFRAME_THRESH:
+            usFrame = mn.idxmax()
+            ratio.loc[:,usFrame] = 0    # blank out the biggest response.
+    else:
+        ratio.loc[:,usFrame] = 0    # blank out the defined US response.
+
+    idxmax = ratio.loc[:, sortStartFrame:sortEndFrame].idxmax( axis = 1 )
+    sortedIdx = idxmax.sort_values().index
+    sortedRatio = ratio.reindex( sortedIdx )
+    fig = plt.figure( figsize = (12,4 ))
+    plt.imshow( np.log10( sortedRatio ) )
+    plt.show()
+
+    return psth, sdev
+
+def responderStats( df, frames ):
+    '''
+    Report 
+        - fraction of cells responding (in a given window?),
+            - Criteria for response: ampl, hit trial rate, window.
+        - Hit trial rate
+        - Mean Amplitude of response
+            - Overall mean
+            - Mean when it is a hit trial.
+        - Tau of response
+    '''
+    return 0
+
+def timeCellStats( df, frames ):
+    '''
+    '''
+    return 0
 
 if __name__ == '__main__':
     main()
